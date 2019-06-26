6e96bd7050cd9bdc8411d2df461efb1463dd8cf4
YOUR NAME <adien330@yahoo.com>
2019-06-25 20:02:03 -0400

    made routes and tested with server

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,20 @@
 class Api::ProductsController < ApplicationController
+
+
+  def show 
+    @product = Product.all
+    render 'food_to_come.json.jb'
+  end 
+
+  def drop 
+    @product = Product.first 
+    render "food_to_send.json.jb"
+  end 
+
+  def store 
+    @product = Product.last
+    render "food_to_get_at_store"
+  end 
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   namespace :api do
   #   get "/photos" => "photos#index"
 
-  
+  get '/food_to_come' => "products#show"
+
+  get '/food_to_get_at_store' => "products#store"
+
+  get '/food_to_send' => "products#drop"
+
+
   end
 end
