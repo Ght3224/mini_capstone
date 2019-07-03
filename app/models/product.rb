ad04f52b3e84bfc6d6f0e65875aea67500f31a4c
YOUR NAME <adien330@yahoo.com>
2019-07-02 20:22:00 -0400

    exercise with model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,33 @@
 class Product < ApplicationRecord
+
+
+
+  def is_discounted?
+
+
+    if price.to_i < 10.to_i
+       true 
+    else 
+       false 
+    end 
+
+
+
+  end 
+
+
+  def tax
+
+   tax = price.to_f * 0.09
+
+  end 
+
+
+  def total 
+
+    total = price.to_f + tax.to_f
+
+  end 
+
+
 end
